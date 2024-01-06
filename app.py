1afa4f3c2d681d209825d3a0b91437cc3cfb44a2
davidtabor0 <dtabor@calstatela.edu>
2024-01-05 17:48:55 -0800

    Added counting to the macro variables

--- app.py
+++ app.py
@@ -2,36 +2,42 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+carbs = 0
+fat = 0
+protein = 0
+
 # routes bind to the function below its definition.
-@app.route('/')
 @app.route('/home')
 def hello():
     return render_template('home.html')
 
-@app.route('/dog')
-def dog():
-    carbs = 100
-    fats = 150
-    proteins = 200
-    return render_template('tracking.html', carbs=carbs, fats=fats, proteins=proteins)
+@app.route('/')
+@app.route('/tracking') 
+def tracking():
+    global carbs, fat, protein
+    return render_template('tracking.html', carbs=carbs, fat=fat, protein=protein)
 
 @app.route('/submit-fat', methods=['POST'])
 def submit_fat():
     fat = int(request.form['fatInput'])
-    print("Fat eaten:", fat)
-    return redirect(url_for('dog'))
+    print("Fat eaten:", fat) # TESTING
+    return redirect(url_for('tracking'))
     
 @app.route('/submit-carbs', methods=['POST'])
 def submit_carbs():
-    carbs = int(request.form['carbsInput'])
-    print("Carbs eaten:", carbs)
-    return redirect(url_for('dog'))
+    global carbs
+    new_carbs = int(request.form['carbsInput'])
+    print("before", carbs)
+    carbs += new_carbs
+    print("Carbs eaten:", new_carbs) # TESTING
+    print("after", carbs)
+    return redirect(url_for('tracking')) # <- item in quotes is the name of the function for that route.
 
 @app.route('/submit-protein', methods=['POST'])
 def submit_protein():
     protein = int(request.form['proteinInput'])
-    print("Carbs eaten:", protein)
-    return redirect(url_for('dog'))
+    print("Carbs eaten:", protein) # TESTING
+    return redirect(url_for('tracking'))
 
 # If this file is run to launch the flask server, instead of using commands, then the server launches in debug.
 if __name__ == '__main__':
