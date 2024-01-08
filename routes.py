df6f83314bbedc339646b4bd8053cb3056da2400
davidtabor0 <dtabor@calstatela.edu>
2024-01-08 15:55:48 -0800

    Updated existing forms to use WTForms

--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from CollectionManager import CollectionManager
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 
 # Initialize a collection under DB name 'MacroTracker' and collection name 'testing123'
@@ -19,22 +19,32 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Register')
 
+class IntegerForm(FlaskForm):
+    number = IntegerField()
+    submit = SubmitField('Submit')
+
+
 #Initialize blueprint to save the routes to.
 bp = Blueprint('routes', __name__)
 
 #Define routes to the blueprint
+# HOME
 @bp.route('/home')
 def hello():
     return render_template('home.html')
 
+# TRACKING
 @bp.route('/')
 @bp.route('/tracking') 
 def tracking():
     global my_collection
-    document = my_collection.find_one_doc({})
-    fat, carbs, protein = document['fat'], document['carbs'], document['protein']
-    return render_template('tracking.html', fat=fat, carbs=carbs, protein=protein)
+    fatform = IntegerForm()
+    carbform = IntegerForm()
+    proteinform = IntegerForm()
+    saved_values = my_collection.find_one_doc({})
+    return render_template('tracking.html', saved_values=saved_values, carbform=carbform, proteinform=proteinform, fatform=fatform)
 
+# LOGIN
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -46,6 +56,7 @@ def login():
         return redirect('/tracking')  # Send the user to the main page of interest after they log in.
     return render_template('login.html', form=form)
 
+# REGISTER
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -58,32 +69,44 @@ def register():
         return redirect('/login')
     return render_template('register.html', form=form) 
 
-@bp.route('/submit-fat', methods=['POST'])
-def submit_fat():
+# SUBMIT FAT
+@bp.route('/submitfat', methods=['POST'])
+def submitfat():
     global my_collection
     document = my_collection.find_one_doc({})
-    fat = document['fat']
-    new_fat = int(request.form['fatInput'])
-    updated_fat = fat + new_fat
-    my_collection.update_one_doc(document, {'$set': {'fat': updated_fat}})
-    return redirect(url_for('routes.tracking')) # <- item in quotes is the name of the function for that route.
+    current_fat = document['fat']
+    form = IntegerForm()
+    if form.validate_on_submit():  # Data processing when submitted.
+        new_fat = form.number.data
+        updated_fat = current_fat + new_fat
+        my_collection.update_one_doc(document, {'$set': {'fat': updated_fat}})
+        return redirect(url_for('routes.tracking'))
+    return redirect(url_for('routes.tracking')) 
 
+# SUBMIT CARBS
 @bp.route('/submit-carbs', methods=['POST'])
 def submit_carbs():
     global my_collection
     document = my_collection.find_one_doc({})
-    carbs = document['carbs']
-    new_carbs = int(request.form['carbsInput'])
-    updated_carbs = carbs + new_carbs
-    my_collection.update_one_doc(document, {'$set': {'carbs': updated_carbs}})
-    return redirect(url_for('routes.tracking')) # <- item in quotes is the name of the function for that route.
+    current_carbs = document['carbs']
+    form = IntegerForm()
+    if form.validate_on_submit():  # Data processing when submitted.
+        new_carbs = form.number.data
+        updated_carbs = current_carbs + new_carbs
+        my_collection.update_one_doc(document, {'$set': {'carbs': updated_carbs}})
+        return redirect(url_for('routes.tracking'))
+    return redirect(url_for('routes.tracking'))
 
+# SUBMIT PROTEIN
 @bp.route('/submit-protein', methods=['POST'])
 def submit_protein():
     global my_collection
     document = my_collection.find_one_doc({})
-    protein = document['protein']
-    new_protein = int(request.form['proteinInput'])
-    updated_protein = protein + new_protein
-    my_collection.update_one_doc(document, {'$set': {'protein': updated_protein}})
-    return redirect(url_for('routes.tracking'))
+    current_protein = document['protein']
+    form = IntegerForm()
+    if form.validate_on_submit():  # Data processing when submitted.
+        new_protein = form.number.data
+        updated_protein = current_protein + new_protein
+        my_collection.update_one_doc(document, {'$set': {'protein': updated_protein}})
+        return redirect(url_for('routes.tracking'))
+    return redirect(url_for('routes.tracking')) 
